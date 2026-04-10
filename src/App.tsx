60e5439297e2d8786fbf02404846735c75369ad6
Supervisor de Vendas <sup.vendas@DCSTEULA.local>
2026-04-10 11:07:21 -0300

    fix: add basename to BrowserRouter for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/sacsteula">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/vendas" element={<Vendas />} />
